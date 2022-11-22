5a14ecf03aa62908376aeef3b9a39e0c083cab50
jvyo05 <jesse86e@gmail.com>
2022-11-21 22:51:39 -0500

    uat added

--- prod.tf
+++ prod.tf
@@ -1,12 +1,12 @@
 module "main-vpc" {
   source     = "../modules/vpc"
-  ENV        = "prod"
+  ENV        = "uat"
   AWS_REGION = var.AWS_REGION
 }
 
 module "instances" {
   source         = "../modules/instances"
-  ENV            = "prod"
+  ENV            = "uat"
   VPC_ID         = module.main-vpc.vpc_id
   PUBLIC_SUBNETS = module.main-vpc.public_subnets
 }
